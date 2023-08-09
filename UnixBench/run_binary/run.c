aca2f687ea15875dc4a5bccbc1a62a9b7d799728
materialofmouse <materialofmouse@gmail.com>
2023-08-09 11:25:06 +0900

    WIP

--- UnixBench/run_binary/run.c
+++ UnixBench/run_binary/run.c
@@ -13,39 +13,47 @@ char pgms[5][100] = {
 	"execl",
 	"fstime"
 };
+//default option
 char *options[6][3] = {
-	{"/home/mouse/byte-unixbench/UnixBench/capsicum_pgms/pipe", "10", NULL},
-	{"/home/mouse/byte-unixbench/UnixBench/capsicum_pgms/syscall", "10", NULL},
-	{"/home/mouse/byte-unixbench/UnixBench/capsicum_pgms/context1", "10", NULL},
-	{"/home/mouse/byte-unixbench/UnixBench/capsicum_pgms/spawn", "30", NULL},
-	{"/home/mouse/byte-unixbench/UnixBench/capsicum_pgms/execl", "30", NULL},
-	{"/home/mouse/byte-unixbench/UnixBench/capsicum_pgms/fstime", "-c -t 30 -d './testDir' -b 1024 -m 2000", NULL},
+	{"pipe", "10", NULL},
+	{"syscall", "10", NULL},
+	{"context1", "10", NULL},
+	{"spawn", "30", NULL},
+	{"execl", "30", NULL},
+	{"fstime", "-c -t 30 -d './testDir' -b 1024 -m 2000", NULL},
 };
 
 char* result = "/home/mouse/byte-unixbench/UnixBench/run_binary/result";
 	
-int main(){
+int main(int argc, char *argv[]){
 	struct timeval startTime, endTime;
 	struct timeval allStartTime, allEndTime;
 	int status;
-
-	//child process
+	int iteration = 0;
+	
+	if (argc <= 3){
+		printf("args\n");
+		return 1;
+	}
+	
+	iteration = argc[1];
 	gettimeofday(&allStartTime, 0);
 	int i, j = 0;
 	for (j = 0; j < 6; j++){
-		for (i = 0; i < 1; i++){
-			printf("program: %s\n",options[j][0]);
+		printf("program: %s\n",options[j][0]);
+		for (i = 0; i < 10; i++){
 			pid_t pid = fork();
 			gettimeofday(&startTime, NULL);
+			//child process
 			if (pid == 0){
 				if(execv(options[j][0], options[j]) == -1){
 					perror("execve");
 				}
 			}
 		//parent process
-		waitpid(pid, &status, 0);
-		gettimeofday(&endTime, NULL);
-		printf("%lf\n", (float)(endTime.tv_sec - startTime.tv_sec) + (float)(endTime.tv_usec - startTime.tv_usec)/100000);
+			waitpid(pid, &status, 0);
+			gettimeofday(&endTime, NULL);
+			printf("%lf\n", (float)(endTime.tv_sec - startTime.tv_sec) + (float)(endTime.tv_usec - startTime.tv_usec)/100000);
 		}
 		printf("------------------\n");
 	}
