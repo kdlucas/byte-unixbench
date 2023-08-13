1164f003c167a0125c149f1b75f70a8c7677b27f
materialofmouse <materialofmouse@gmail.com>
2023-08-13 22:51:02 +0900

    fix

--- UnixBench/Run_capsicum.c
+++ UnixBench/Run_capsicum.c
@@ -25,7 +25,7 @@ extern char **environ;
 int main(int argc, char *argv[]){
 	struct timeval startTime, endTime;
 	struct timeval allStartTime, allEndTime;
-	int iteration = 0;
+	//int iteration = 0;
 	char pgms[6][256] = {
 		"pipe",
 		"syscall",
@@ -55,25 +55,32 @@ int main(int argc, char *argv[]){
 	gettimeofday(&allStartTime, 0);
 	int i, j = 0;
 	for (j = 0; j < 6; j++){
+		int status;
 		char binary[256] = "";
 		snprintf(binary, sizeof binary, "%s/%s", CURRENT_PATH, pgms[j]);
 		printf("binary %s\n", binary);
-		char *const binary_options = { binary, options[j][0], options[j][1]};
-
+		
+		char *binary_options[3]; 
+		binary_options[0] = binary;
+		binary_options[1] = options[j][0];
+		binary_options[2] = NULL;
+		printf("iterations: %d\n",iteration);
 		for (i = 0; i < iteration; i++){
 			pid_t pid = fork();
 			gettimeofday(&startTime, NULL);
 			//child process
 			if (pid == 0){
-				if(execv(binary_options[0], binary_options) == -1){
+				if(execve(binary_options[0], binary_options, environ)== -1){
 					perror("execve");
 				}
 			}
-			else if (pid > 0){
-				//parent process
-				int status;
-				waitpid(pid, &status, 0);
-				
+			
+			//parent process
+			if (pid = waitpid(pid, &status, 0) < 0){
+				perror("wait");
+				return 1;
+			}
+			if (WIFEXITED(status)) {
 				gettimeofday(&endTime, NULL);
 				printf("%lf\n", (float)(endTime.tv_sec - startTime.tv_sec) + (float)(endTime.tv_usec - startTime.tv_usec)/100000);
 			}
